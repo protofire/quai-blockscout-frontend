948f478241ae59075fb25634027ef2705a4fe381
Nikolai Evseev <evseevnn@gmail.com>
2024-04-24 10:57:03 +0100

    Update .env.production file with new network and API configurations

--- lib/hooks/useShards.tsx
+++ lib/hooks/useShards.tsx
@@ -9,7 +9,12 @@ import type { ShardId, ShardInfo } from 'types/shards';
 
 import config from 'configs/app';
 
-type SubscriptionParams = {channelTopic: string; event: string; params?: Record<string, string>; onMessage: (shardId: ShardId, msg: unknown) => void};
+type SubscriptionParams = {
+  channelTopic: string;
+  event: string;
+  params?: Record<string, string>;
+  onMessage: (shardId: ShardId, msg: unknown) => void;
+};
 
 type UseShardsResult = {
   shardId?: ShardId;
@@ -30,101 +35,141 @@ export default function useShards(): UseShardsResult {
   const queryStringParams = useSearchParams();
   const router = useRouter();
   const shardsConfig = getFeaturePayload(config.features.shards);
-  const shards = React.useMemo(() => shardsConfig?.shards || {}, [ shardsConfig ]);
+  const shards = React.useMemo(
+    () => shardsConfig?.shards || {},
+    [ shardsConfig ],
+  );
   const defaultShardId = Object.keys(shards)[0];
 
-  const shardId = queryStringParams.get('shard') as ShardId || defaultShardId;
+  const shardId = (queryStringParams.get('shard') as ShardId) || defaultShardId;
 
-  const setActiveShardId = useCallback(async(shardId: ShardId) => {
-    if (!shardId) {
-      return;
-    }
-
-    await router.push({ pathname: router.pathname, query: { ...router.query, shard: shardId } });
-    router.reload();
-  }, [ router ]);
-
-  const getUrlWithShardId = useCallback((url: string) => {
-    const shardablePages = getFeaturePayload(config.features.shards)?.pages;
-    const isShardable = shardablePages?.find((page) => url.startsWith(page)) !== undefined;
-
-    if (isShardable && shardId) {
-      const baseUrl = [
-        config.app.protocol || 'http',
-        '://',
-        config.app.host || 'localhost',
-        config.app.port && ':' + config.app.port,
-      ].filter(Boolean).join('');
-      const newUrl = new URL(url as string, baseUrl);
-
-      // Add shardId to query params for tabs
-      if (!newUrl.searchParams.has('shard') || !newUrl.searchParams.get('shard')) {
-        newUrl.searchParams.append('shard', shardId);
+  const setActiveShardId = useCallback(
+    async(shardId: ShardId) => {
+      if (!shardId) {
+        return;
       }
 
-      return newUrl.href;
-    }
+      await router.push({
+        pathname: router.pathname,
+        query: { ...router.query, shard: shardId },
+      });
+      router.reload();
+    },
+    [ router ],
+  );
 
-    return url;
-  }, [ shardId ]);
+  const getUrlWithShardId = useCallback(
+    (url: string) => {
+      const shardablePages = getFeaturePayload(config.features.shards)?.pages;
+      const isShardable =
+        shardablePages?.find((page) => url.startsWith(page)) !== undefined;
 
-  const extractShardIdFromAddress = useCallback((address: string) => {
+      if (isShardable && shardId) {
+        const baseUrl = [
+          config.app.protocol || 'http',
+          '://',
+          config.app.host || 'localhost',
+          config.app.port && ':' + config.app.port,
+        ]
+          .filter(Boolean)
+          .join('');
+        const newUrl = new URL(url as string, baseUrl);
 
-    // For understand what shard actually related to address we need take 2 first symbols from address
-    const marker = address.slice(0, 4).toLowerCase();
-    const allShardsIds = Object.keys(shards);
-
-    // Then we can check marker with shards addressesFrom
-    const shardId = allShardsIds.sort().reverse().find((shardId) => {
-      const addressesFrom = shards[shardId].addressesFrom;
-      return marker >= addressesFrom;
-    });
-
-    return shardId || defaultShardId;
-  }, [ shards, defaultShardId ]);
-
-  const subscribeOnTopicMessage = useCallback(({ channelTopic, event, params, onMessage }: SubscriptionParams) => {
-    // Init sockets if they are not initialized
-    if (!sockets.length) {
-      const sockets: Array<Socket> = Object.keys(shards).map((shardId: ShardId) => {
-        const wsUrl = new URL(`${ config.api.socket }${ config.api.basePath }/socket/v2`);
-        const shard = shards[shardId];
-        const shardHost = shard?.apiHost;
-        if (shardHost) {
-          // Replace host
-          wsUrl.host = shardHost;
+        // Add shardId to query params for tabs
+        if (
+          !newUrl.searchParams.has('shard') ||
+          !newUrl.searchParams.get('shard')
+        ) {
+          newUrl.searchParams.append('shard', shardId);
         }
 
-        const socketInstance = new Socket(wsUrl.toString());
-        return socketInstance;
-      }, []);
+        return newUrl.href;
+      }
 
-      setSockets(sockets);
-    }
+      return url;
+    },
+    [ shardId ],
+  );
 
-    const channels = sockets.map((socket, index) => {
-      // eslint-disable-next-line no-console
-      console.log(`Subscribing to ${ channelTopic } channel on shard ${ index }`);
-      socket.connect();
-      const channel = socket.channel(channelTopic, params);
-      // eslint-disable-next-line no-console
-      channel.join().receive('ok', () => console.log(`Successfully joined ${ channelTopic } channel on shard ${ index }`));
-      channel.on(event, (msg) => {
-        const shardId = Object.keys(shards)[index];
-        onMessage(shardId, msg);
+  const extractShardIdFromAddress = useCallback(
+    (address: string) => {
+      // For understand what shard actually related to address we need take 2 first symbols from address
+      const marker = address.slice(0, 4).toLowerCase();
+
+      // Sort shards by addressesFrom field desc
+      const sortFn = (a: ShardInfo, b: ShardInfo) => {
+        const aAddressesFrom = parseInt(a.addressesFrom, 16);
+        const bAddressesFrom = parseInt(b.addressesFrom, 16);
+        return aAddressesFrom - bAddressesFrom;
+      };
+      const allShards = Object.values(shards).sort(sortFn);
+
+      // Then we can check marker with shards addressesFrom
+      const shard = allShards.find((shard) => {
+        const addressesFrom = shard.addressesFrom;
+        return marker >= addressesFrom;
       });
-      return channel;
-    });
 
-    setChannels(channels);
-  }, [ shards, sockets ]);
+      return shard?.id || defaultShardId;
+    },
+    [ shards, defaultShardId ],
+  );
+
+  const subscribeOnTopicMessage = useCallback(
+    ({ channelTopic, event, params, onMessage }: SubscriptionParams) => {
+      // Init sockets if they are not initialized
+      if (!sockets.length) {
+        const sockets: Array<Socket> = Object.keys(shards).map(
+          (shardId: ShardId) => {
+            const wsUrl = new URL(
+              `${ config.api.socket }${ config.api.basePath }/socket/v2`,
+            );
+            const shard = shards[shardId];
+            const shardHost = shard?.apiHost;
+            if (shardHost) {
+              // Replace host
+              wsUrl.host = shardHost;
+            }
+
+            const socketInstance = new Socket(wsUrl.toString());
+            return socketInstance;
+          },
+          [],
+        );
+
+        setSockets(sockets);
+      }
+
+      const channels = sockets.map((socket, index) => {
+        // eslint-disable-next-line no-console
+        console.log(`Subscribing to ${ channelTopic } channel on shard ${ index }`);
+        socket.connect();
+        const channel = socket.channel(channelTopic, params);
+        // eslint-disable-next-line no-console
+        channel
+          .join()
+          .receive('ok', () => {
+            // eslint-disable-next-line no-console
+            console.log(`Joined to ${ channelTopic } channel on shard ${ index }`);
+          });
+        channel.on(event, (msg) => {
+          const shardId = Object.keys(shards)[index];
+          onMessage(shardId, msg);
+        });
+        return channel;
+      });
+
+      setChannels(channels);
+    },
+    [ shards, sockets ],
+  );
 
   useEffect(() => {
     return () => {
       channels.forEach((channel) => channel.leave());
       sockets.forEach((socket) => socket.disconnect());
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return {
